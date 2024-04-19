5de165cdbb6759cbd07f8d1ab934a3f6f8bf3e1b
N1AKAI <rakansubs@gmail.com>
2024-04-19 16:15:42 +0100

    update

--- src/Repository/TestOnlineRepository.php
+++ src/Repository/TestOnlineRepository.php
@@ -16,14 +16,14 @@ class TestOnlineRepository
   public function getTestsOnline($student_id)
   {
     $stmt = $this->con->prepare("SELECT class_id,
-    test_online_id, test_online_name, duration, score,
+    test_online.test_online_id, test_online_name, duration, score,
     course_name
     FROM test_online
     INNER JOIN course USING (course_id)
-    LEFT JOIN test_online_student USING (test_online_id)
     INNER JOIN class USING (class_id)
     INNER JOIN class_student USING (class_id)
-    WHERE class_student.student_id = ?
+    LEFT JOIN test_online_student USING (student_id)
+    WHERE student_id = 3
     ORDER BY test_online.test_online_id DESC");
     $stmt->bind_param("i", $student_id);
     $stmt->execute();
